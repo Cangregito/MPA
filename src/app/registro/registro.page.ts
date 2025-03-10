d72f7780205da154acb596109435abce26414e26
Cangregito <Jassiel.rr1502@gmail.com>
2025-03-10 12:27:46 -0600

    Se agrego el maquetado de registro

---
+++ src/app/registro/registro.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+  standalone: true,
+  imports: [IonicModule]
+})
+export class RegistroPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
