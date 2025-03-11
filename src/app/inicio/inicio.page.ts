cb95aad88930a2e1bf5a65f746a49a8b98bfaa3c
Langostin <erick.2468.mx@gmail.com>
2025-03-11 11:20:38 -0600

    Se Agregaron card en el inicio principal

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, RouterLink]
 })
 export class InicioPage implements OnInit {
 
