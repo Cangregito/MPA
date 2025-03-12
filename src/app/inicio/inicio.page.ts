1d17584b9928f11051edfb5ed7459f6187740ca6
Langostin <erick.2468.mx@gmail.com>
2025-03-12 07:21:09 -0600

    Se agregaron algunos estilos a inicio ademas de slides temporales

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
+import { IonicSlides } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
   selector: 'app-inicio',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
   imports: [IonicModule, RouterLink]
 })
 export class InicioPage implements OnInit {
+  swiperModules = [IonicSlides];
 
   constructor() { }
 
