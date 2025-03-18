9e83ca45a5591472ca1702c0851a70687100784f
Langostin <erick.2468.mx@gmail.com>
2025-03-18 10:21:41 -0600

    Se agrego la pagina de control de puertas

--- src/app/puertas/puertas.page.ts
+++ src/app/puertas/puertas.page.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-puertas',
   templateUrl: './puertas.page.html',
   styleUrls: ['./puertas.page.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, CommonModule]
 })
 export class PuertasPage implements OnInit {
+  puerta1: boolean = true; // true = cerrada, false = abierta
+  puerta2: boolean = true; // true = cerrada, false = abierta
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  togglePuerta(numero: number) {
+    if (numero === 1) {
+      this.puerta1 = !this.puerta1;
+    } else if (numero === 2) {
+      this.puerta2 = !this.puerta2;
+    }
   }
-
 }
